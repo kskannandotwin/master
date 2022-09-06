f11fe68063b6a01c44d5c59f86846630bb72290e
Kannan <kskannan.win@gmail.com>
2022-09-06 12:52:43 +0530

    06.09.2022
    chapter 4 complted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,20 @@ import { Component } from "@angular/core";
 export class AppComponent {
   name = 'luis ramirez';
   imgUrl = 'https://picsum.photos/id/237/500/500';
+  images = [
+    'https://picsum.photos/id/237/500/500',
+    'https://picsum.photos/id/237/500/500',
+    'https://picsum.photos/id/237/500/500'
+  ];
   currentDate = new Date();
   cost = 2000;
   temperature = 25.3;
+  pizza = {
+    toppings: ['pepperoni', 'bacon'],
+    size: 'large'
+  }
+  blueClass = false;
+  fontSize = 16;
 
   getName() {
     return this.name;
